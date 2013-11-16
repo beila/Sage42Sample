b32a6c400486919bc6eb273adb392070e8bd4975
Corey Scott <corey.scott@gmail.com>
2013-11-17 01:15:57 +0800

    -refactored to separate java and android code. -change from maven build to gradle -added support for junit tests on the java porject -added support for robotium tests on the app project

---
+++ app/src/main/java/org/sage42/sample/app/SampleApplication.java
@@ -0,0 +1,11 @@
+package org.sage42.sample.app;
+
+import android.app.Application;
+
+import com.googlecode.androidannotations.annotations.EApplication;
+
+@EApplication
+public class SampleApplication extends Application
+{
+    // nothing yet
+}
---
+++ lib/src/main/java/org/sage42/sample/lib/CalculatorModel.java
@@ -0,0 +1,21 @@
+package org.sage42.sample.lib;
+
+/**
+ * This is a contrived example mention to show how to test via gradle and junit.
+ *
+ */
+public class CalculatorModel
+{
+    /**
+     * Returns the sum of the supplied 2 integers.
+     * 
+     * @param operandA
+     * @param operandB
+     * @return
+     */
+    public static int add(final int operandA, final int operandB)
+    {
+        return operandA + operandB;
+    }
+
+}
